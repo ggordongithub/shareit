f68aa933149c674019310b919b8b900884d43d4f
ggordon <geraldgordon4@gmail.com>
2014-09-27 06:45:11 -0400

    adding new files

---
+++ test/unit/shareref/ProspectEmployerControllerSpec.groovy
@@ -0,0 +1,20 @@
+package shareref
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ProspectEmployerController)
+class ProspectEmployerControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
